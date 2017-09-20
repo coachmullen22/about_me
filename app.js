ca3e9d2efc262233ad040e09d7fe449a30b5a932
Jeff <coachmullen@gmail.com>
2017-09-20 07:54:07 -0700

    overnight updates include major css experiments and html edits

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
 'use strict';
 
 var userName = prompt('What is your name?');
+console.log('Value of userName is', userName)
 
 alert('Hi, ' + userName + ' thank you for checking out my bio page!');
 
 //Question 1
 
 var siblings = prompt('Do I have any sisters?').toLowerCase();
-console.log('Do I have any siblings?', siblings);
+console.log('Value of siblings is', siblings);
 
 if (siblings === 'n' || siblings === 'no') {
   // Tell the user they are correct
   alert('I actually have three sisters. And although they were sometimes hard on me, I wouldn\'t have traded any one of them for a brother...most of the time...');
 } else {
     // Tell the user they are wrong
-  alert('That\'s right. I have three amazing sisters. I\'m so lucky to have them!');
+  alert('That\'s right. I have three amazing sisters. I\'m very lucky to have them!');
 }
 
-var spoiledNephews = prompt('I\'m lucky enough to have two little nephews. Do I spoil them?').toLowerCase();
-console.log('Do I have any children?', spoiledNephews);
+var spoiledNephews = prompt('I\'m also fortunate to have two little nephews. Do you think I\'m the type of uncle who would spoil them?').toLowerCase();
+console.log('Value of spoiledNephews is', spoiledNephews);
 
 if (spoiledNephews === 'n' || spoiledNephews === 'no') {
-  alert('My sister\'s don\'t like it, but I actually do. If you could see them, you wouldn\'t be able to control yourself either. They\'re the cutest little guys ever!')
+  alert('Well, my sister\'s don\'t like it very much, but I actually do spoil them. If you could see those two, you would do the same thing. They\'re the cutest little guys ever!')
 } else {
-  alert('I only spoil them a little bit. Mostly I try to explore the world with them.');
+  alert('Well, I only spoil them a little bit. Mostly I try to explore the world with them. It\'s fun seeing the world through their eyes.');
 }
 
-var kingDome = prompt('Have I ever hit a baseball in a Major League stadium?').toLowerCase();
-console.log('Have I ever hit in a major league stadium', kingDome);
+var hasHitStadium = prompt('Have I ever hit a baseball in a Major League stadium?').toLowerCase();
+console.log('Value of hasHitStadium', hasHitStadium);
 
-if (kingDome === 'n' || kingDome === 'no') {
-  alert('Do you doubt my talent? Just kidding.  I actually coached a high school team that got invited to take batting practice in the Kingdome.  I took my turn and had one of my hits bounce over the fence on one hop.  Warning track power is all I ever had... d:)')
+if (hasHitStadium === 'n' || hasHitStadium === 'no') {
+  alert('So you doubt my talent? I wouldn\'t have asked if it wasn\'t true. I once coached a high school team that got invited to take batting practice in the Kingdome. Of course I took my turn and had one of my hits bounce over the fence on one hop. Sadly, I never had anything more than warning track power.')
 } else {
-  alert('You\'re right! I got to take batting practice in the Kingdome with a highschool team I coached.  I actually snuck into the Mariners locker room and sat in Ken Griffey Jr\'s fancy recliner chair. I\m not sure why but he had a baseball card of Matt Sinatro pinned to his locker.')
+  alert('You\'re right! I got to take batting practice in the Kingdome with a highschool team I coached.  I actually snuck into the Mariners locker room and sat in Ken Griffey Jr\'s fancy recliner chair. I\'m not sure why it was there, but he had a baseball card of Matt Sinatro pinned to his locker...that\'s right, Matt Sinatro.')
 }
 
 var marinersFan = prompt('Do I like the Mariners?').toLowerCase();
-console.log('Am I a Mariners fan?', marinersFan);
+console.log('Value of marinersFan is', marinersFan);
 
 if (marinersFan === 'n' || marinersFan === 'no') {
-  alert(userName +'You are correct. I don\'t just like the Mariners, I love my Mariners!');
+  alert(userName +', you are correct...sort of. I don\'t just like the Mariners, I love my Mariners!');
 } else {
-  alert('I grew up loving the Mariners. Even though they cause me great pain, there\'s no sense changing horses in mid-stream')
+  alert('Of course I do! I grew up loving the Mariners. Even though they cause me great pain, there\'s no sense changing horses in mid-stream')
 }
