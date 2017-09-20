69e5f983f2e4eda6190462ec35a2525098ff130f
Jeff <coachmullen@gmail.com>
2017-09-19 18:10:06 -0700

    overdue update

--- app.js
+++ app.js
@@ -6,22 +6,40 @@ alert('Hi, ' + userName + ' thank you for checking out my bio page!');
 
 //Question 1
 
-var answer1 = prompt('Do I have for cats?').toLowerCase();
-console.log('answer 1 is', answer1);
+var siblings = prompt('Do I have any sisters?').toLowerCase();
+console.log('Do I have any siblings?', siblings);
 
-if (answer1 === 'n' || answer1 === 'no') {
+if (siblings === 'n' || siblings === 'no') {
   // Tell the user they are correct
-  alert('You are correct!');
+  alert('I actually have three sisters. And although they were sometimes hard on me, I wouldn\'t have traded any one of them for a brother...most of the time...');
 } else {
     // Tell the user they are wrong
-  alert('You\'re very wrong');
+  alert('That\'s right. I have three amazing sisters. I\'m so lucky to have them!');
 }
 
-var answer2 = prompt('Do I like the Mariners?').toLowerCase();
-console.log('answer2 is', answer2);
+var spoiledNephews = prompt('I\'m lucky enough to have two little nephews. Do I spoil them?').toLowerCase();
+console.log('Do I have any children?', spoiledNephews);
 
-if (answer2 === 'n' || answer2 === 'no') {
-  alert('It may be hard to believe but I love my Mariners!');
+if (spoiledNephews === 'n' || spoiledNephews === 'no') {
+  alert('My sister\'s don\'t like it, but I actually do. If you could see them, you wouldn\'t be able to control yourself either. They\'re the cutest little guys ever!')
 } else {
-  alert('I can\'t help myself. The Mariners have put me through too much misery for me to give up on them now...')
+  alert('I only spoil them a little bit. Mostly I try to explore the world with them.');
+}
+
+var kingDome = prompt('Have I ever hit a baseball in a Major League stadium?').toLowerCase();
+console.log('Have I ever hit in a major league stadium', kingDome);
+
+if (kingDome === 'n' || kingDome === 'no') {
+  alert('Do you doubt my talent? Just kidding.  I actually coached a high school team that got invited to take batting practice in the Kingdome.  I took my turn and had one of my hits bounce over the fence on one hop.  Warning track power is all I ever had... d:)')
+} else {
+  alert('You\'re right! I got to take batting practice in the Kingdome with a highschool team I coached.  I actually snuck into the Mariners locker room and sat in Ken Griffey Jr\'s fancy recliner chair. I\m not sure why but he had a baseball card of Matt Sinatro pinned to his locker.')
+}
+
+var marinersFan = prompt('Do I like the Mariners?').toLowerCase();
+console.log('Am I a Mariners fan?', marinersFan);
+
+if (marinersFan === 'n' || marinersFan === 'no') {
+  alert(userName +'You are correct. I don\'t just like the Mariners, I love my Mariners!');
+} else {
+  alert('I grew up loving the Mariners. Even though they cause me great pain, there\'s no sense changing horses in mid-stream')
 }
