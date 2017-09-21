807c42e12567d7d3a24c7c06e41f0950dac7dd72
jjkusowski <jjkusowski@yahoo.com>
2017-09-21 13:59:20 -0700

    question 3 made into a function

--- app.js
+++ app.js
@@ -33,14 +33,18 @@ function quesion2() {
 question2();
 
 //Question 3
-var hasHitStadium = prompt('Have I ever hit a baseball in a Major League stadium?').toLowerCase();
-console.log('Value of hasHitStadium', hasHitStadium);
 
-if (hasHitStadium === 'n' || hasHitStadium === 'no') {
-  alert('Are you saying I don\'t look like a big-leaguer? Hmm, perhaps I don\'t. But I once coached a high school team that got invited to take batting practice in the Kingdome. Naturally, I took my turn at-bat, and one of my hits one-hopped over the fence. Sadly, I never had anything more than warning track power.');
-} else {
-  alert('You\'re right! I got to take batting practice in the Kingdome with a highschool team I coached.  I actually snuck into the Mariners locker room and sat in Ken Griffey Jr\'s fancy recliner chair. I\'m not sure why it was there, but he had a baseball card of Matt Sinatro pinned to his locker...that\'s right, Matt Sinatro.');
-}
+function question3() {
+  var hasHitStadium = prompt('Have I ever hit a baseball in a Major League stadium?').toLowerCase();
+  console.log('Value of hasHitStadium', hasHitStadium);
+
+  if (hasHitStadium === 'n' || hasHitStadium === 'no') {
+    alert('Are you saying I don\'t look like a big-leaguer? Hmm, perhaps I don\'t. But I once coached a high school team that got invited to take batting practice in the Kingdome. Naturally, I took my turn at-bat, and one of my hits one-hopped over the fence. Sadly, I never had anything more than warning track power.');
+  } else {
+    alert('You\'re right! I got to take batting practice in the Kingdome with a highschool team I coached.  I actually snuck into the Mariners locker room and sat in Ken Griffey Jr\'s fancy recliner chair. I\'m not sure why it was there, but he had a baseball card of Matt Sinatro pinned to his locker...that\'s right, Matt Sinatro.');
+  }
+  }
+question3();
 
 //Question 4
 var marinersFan = prompt('Do I like the Mariners?').toLowerCase();
